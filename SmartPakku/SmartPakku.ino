4f6ff13042114ba8b8c8450438332a4e8025c53f
ResoluteDreamer <resolutedreamer@live.com>
2015-01-03 12:14:56 -0800

    3rd Commit - Reformatted Text
    Changed the program so it would be more read-able to me.

--- SmartPakku/SmartPakku.ino
+++ SmartPakku/SmartPakku.ino
@@ -25,82 +25,91 @@ unsigned long lastSent;
 // This function is called when nRF8001 responds with the temperature
 void temperatureHandler(float tempC)
 {
-  Serial.println("received temperature");
-  temperatureC = tempC;
+	Serial.println("received temperature");
+	temperatureC = tempC;
 }
 
 // Generic event handler, here it's just for debugging all received events
 void eventHandler(nRFEvent *event)
 {
-  Serial.println("event handler");
-  nrf->debugEvent(event);
+	Serial.println("event handler");
+	nrf->debugEvent(event);
 }
 
-void setup() {
-  temperatureC = 0.0;
-  pipeStatusReceived = 0;
-  lastSent = 0;
-  
-  Serial.begin(115200);
-  Serial.println("Hello");
-  
-  // nRF8001 class initialized with pin numbers
-  nrf = new nRF8001(RESET_PIN, REQN_PIN, RDYN_PIN);
+void setup()
+{
+	temperatureC = 0.0;
+	pipeStatusReceived = 0;
+	lastSent = 0;
 
-  // Register event handles
-  nrf->setEventHandler(&eventHandler);
-  nrf->setTemperatureHandler(&temperatureHandler);
-  if ((nrf->setup(setup_msgs, NB_SETUP_MESSAGES)) == cmdSuccess) {
-    Serial.println("SUCCESS");
-  } else {
-    Serial.println("FAIL");
-    while (1);
-  }
-  
-  // These functions merely request device address and temperature,
-  // actual responses are asynchronous. They'll return error codes
-  // if somehow the request itself failed, for example because
-  // the device is not ready for these commands.
-  nrf->getDeviceAddress();
-  nrf->poll();
-  nrf->getTemperature();
-  nrf->poll();
-  
-  if (temperatureC > 0.0) {
-    Serial.print("Temperature: ");
-    Serial.println(temperatureC, 2);
-  }
-  
-  nrf->connect(0, 32);
+	Serial.begin(115200);
+	Serial.println("Hello");
+
+	// nRF8001 class initialized with pin numbers
+	nrf = new nRF8001(RESET_PIN, REQN_PIN, RDYN_PIN);
+
+	// Register event handles
+	nrf->setEventHandler(&eventHandler);
+	nrf->setTemperatureHandler(&temperatureHandler);
+	if ((nrf->setup(setup_msgs, NB_SETUP_MESSAGES)) == cmdSuccess)
+	{
+		Serial.println("SUCCESS");
+	}
+	else
+	{
+		Serial.println("FAIL");
+		while (1);
+	}
+
+	// These functions merely request device address and temperature,
+	// actual responses are asynchronous. They'll return error codes
+	// if somehow the request itself failed, for example because
+	// the device is not ready for these commands.
+	nrf->getDeviceAddress();
+	nrf->poll();
+	nrf->getTemperature();
+	nrf->poll();
+
+	if (temperatureC > 0.0) {
+	Serial.print("Temperature: ");
+	Serial.println(temperatureC, 2);
+	}
+
+	nrf->connect(0, 32);
 }
 
-void loop() {
-  Serial.println("polling");
-  
-  // Polling will block - times out after 2 seconds
-  nrf->poll(2000);
-  
-  // If heart rate pipe is open
-  if (nrf->isPipeOpen(PIPE_HEART_RATE_HEART_RATE_MEASUREMENT_TX) && (millis() - lastSent) > 1000 && temperatureC > 0.0 && nrf->creditsAvailable()) {
-    Serial.println("ready to send data");
-    uint8_t temp[2];
-    temp[0] = 0;
-    temp[1] = round(temperatureC);
-    
-    nrf->sendData(PIPE_HEART_RATE_HEART_RATE_MEASUREMENT_TX, 2, (uint8_t *)&temp);
-    lastSent = millis();
-    uint8_t bat = 78;
-    
-    // If battery pipe is open
-    if (nrf->isPipeOpen(PIPE_BATTERY_BATTERY_LEVEL_TX) && nrf->creditsAvailable()) {
-      nrf->sendData(PIPE_BATTERY_BATTERY_LEVEL_TX, 1, &bat);
-    }
-    
-    // get new temperature
-    nrf->getTemperature();
-  } else if (nrf->getConnectionStatus() == Disconnected) {
-    Serial.println("Reconnecting");
-    dataSent = 0;
-    nrf->connect(0, 32);
-  }
+void loop()
+{
+	Serial.println("polling");
+
+	// Polling will block - times out after 2 seconds
+	nrf->poll(2000);
+
+	// If heart rate pipe is open
+	if (nrf->isPipeOpen(PIPE_HEART_RATE_HEART_RATE_MEASUREMENT_TX) && (millis() - lastSent) > 1000 && temperatureC > 0.0 && nrf->creditsAvailable())
+	{
+		Serial.println("ready to send data");
+		uint8_t temp[2];
+		temp[0] = 0;
+		temp[1] = round(temperatureC);
+
+		nrf->sendData(PIPE_HEART_RATE_HEART_RATE_MEASUREMENT_TX, 2, (uint8_t *)&temp);
+		lastSent = millis();
+		uint8_t bat = 78;
+
+		// If battery pipe is open
+		if (nrf->isPipeOpen(PIPE_BATTERY_BATTERY_LEVEL_TX) && nrf->creditsAvailable())
+		{
+			nrf->sendData(PIPE_BATTERY_BATTERY_LEVEL_TX, 1, &bat);
+		}
+
+		// get new temperature
+		nrf->getTemperature();
+	}
+	else if (nrf->getConnectionStatus() == Disconnected)
+	{
+		Serial.println("Reconnecting");
+		dataSent = 0;
+		nrf->connect(0, 32);
+	}
 }
